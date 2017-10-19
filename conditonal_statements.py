2caec667265be434ad9523a0acf0cdeeb37a4fd4
eetishan <eeti3084@gmail.com>
2017-10-18 22:53:36 -0400

    conditiontal statement changed

--- conditonal_statements.py
+++ conditonal_statements.py
@@ -1,31 +1,31 @@
-# language ='java'
-# if language == 'python':
-#     print('Language is python')
-# elif language == 'java':
-#     print('Language is java')
+language ='java'
+if language == 'python':
+    print('Language is python')
+elif language == 'java':
+    print('Language is java')
+else:
+    print('No match')
+# switch case statement can be added by adding more elif
+
+# user='Admin'
+# logged_in=True
+
+# # if user == 'Admin' or logged_in:
+#     print('Admin page')
 # else:
-#     print('No match')
-#switch case statement can be added by adding more elif
+#     print('Brad creds')
 
-user='Admin'
-logged_in=True
+# a=[1,2,3]
+# b=[1,2,3]
 
-if user == 'Admin' or logged_in:
-    print('Admin page')
-else:
-    print('Brad creds')
+# print(id(a))
+# print(id(b))
+# print(a is b)
 
-a=[1,2,3]
-b=[1,2,3]
-
-print(id(a))
-print(id(b))
-print(a is b)
-
-condition=none
-if condition:
-    print('Evaluated to True')
-else:
+# condition=none
+# if condition:
+#     print('Evaluated to True')
+# else:
 
 
 #is comparsion
